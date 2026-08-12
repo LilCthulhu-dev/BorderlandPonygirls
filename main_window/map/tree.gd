eb5c0d528306a2bff5b6f3c5d591ab7b3d480126
LilCthulhu <316244590+LilCthulhu-dev@users.noreply.github.com>
2026-08-12 18:47:09 +0200

    added screenshots

--- main_window/map/tree.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	if Utils.rng.randi_range(0, 1) == 1:
-		scale.x *= -1
--- mountain.gd
+++
@@ -1,5 +0,0 @@
-extends Sprite2D
-
-func _ready() -> void:
-	if Utils.rng.randi_range(0, 1) == 1:
-		scale.x *= -1
