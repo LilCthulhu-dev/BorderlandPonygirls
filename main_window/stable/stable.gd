2689faf5a730eb605a0c715afa5f797bfcc539f4
LilCthulhu <316244590+LilCthulhu-dev@users.noreply.github.com>
2026-08-14 19:32:33 +0200

    flags with icon and description now are displayed as quest icons in stable window

--- main_window/stable/stable.gd
+++ main_window/stable/stable.gd
@@ -1,9 +1,12 @@
 extends StoryPage
 
-var pony_btn = preload("res://main_window/stable/pony_btn.tscn")
+const PONY_BTN = preload("uid://ck8d7c8olwxhh")
+const QUEST_ICON = preload("uid://c3jxucw5l3to6")
+
 @onready var titel_label: Label = %TitelLabel
 @onready var active_list: GridContainer = %ActiveList
 @onready var resting_list: GridContainer = %RestingList
+@onready var quest_icons: GridContainer = %QuestIcons
 
 func _ready() -> void:
 	super()
@@ -15,10 +18,23 @@ func _on():
 	_update_ponygirls()
 
 func _update_ponygirls() -> void:
-	_clear_container(active_list)
-	_clear_container(resting_list)
+	Utils.clear_container(active_list)
+	Utils.clear_container(resting_list)
+	Utils.clear_container(quest_icons)
+	_add_ponygirls()
+	_add_quest_icons()
+
+func _add_quest_icons() -> void:
+	for flag: Flag in FlagsManager.flags.values():
+		if not flag.add_quest_icon():
+			continue
+		var icon = QUEST_ICON.instantiate()
+		icon.flag = flag
+		quest_icons.add_child(icon)
+
+func _add_ponygirls() -> void:
 	for pony in PonygirlManager.ponygirls:
-		var btn := pony_btn.instantiate()
+		var btn := PONY_BTN.instantiate()
 		btn.ponygirl = pony
 		if pony.active:
 			btn.active_btn = true
@@ -27,15 +43,10 @@ func _update_ponygirls() -> void:
 			btn.active_btn = false
 			resting_list.add_child(btn)
 	while active_list.get_child_count() < PonygirlManager.MAX_ACTIVE:
-		var btn := pony_btn.instantiate()
+		var btn := PONY_BTN.instantiate()
 		btn.active_btn = true
 		active_list.add_child(btn)
 	while resting_list.get_child_count() < PonygirlManager.MAX_RESTING:
-		var btn := pony_btn.instantiate()
+		var btn := PONY_BTN.instantiate()
 		btn.active_btn = false
 		resting_list.add_child(btn)
-
-func _clear_container(container: Container) -> void:
-	for child in container.get_children():
-		container.remove_child(child)
-		child.queue_free()
