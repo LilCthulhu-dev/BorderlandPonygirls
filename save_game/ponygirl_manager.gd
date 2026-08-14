1bd1c959c750bc62cf7bb5b5ee2cd20ec110b8bf
LilCthulhu <316244590+LilCthulhu-dev@users.noreply.github.com>
2026-08-14 13:03:23 +0200

    removed unused function

--- save_game/ponygirl_manager.gd
+++ save_game/ponygirl_manager.gd
@@ -174,17 +174,3 @@ static func add_ponygirl(pony: Ponygirl) -> void:
 	new_pony.active = active_slots_free()
 	ponygirls.append(new_pony)
 	focused_ponygirl = new_pony
-
-
-## Fully configured instance (no second init). Used by archetype AddPonygirl.
-static func add_ponygirl_instance(pony: Ponygirl) -> void:
-	if pony == null:
-		push_error("PonygirlManager.add_ponygirl_instance: pony is null")
-		return
-	if not slots_free():
-		return
-	if pony.id.is_empty():
-		pony.id = str(ResourceUID.create_id())
-	pony.active = active_slots_free()
-	ponygirls.append(pony)
-	focused_ponygirl = pony
