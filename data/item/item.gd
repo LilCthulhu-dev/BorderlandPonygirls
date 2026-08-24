24f099b6943fb4cc249de195cc4a87fa55d71c3e
LilCthulhu <316244590+LilCthulhu-dev@users.noreply.github.com>
2026-08-24 22:19:12 +0200

    added new items

--- data/item/item.gd
+++ data/item/item.gd
@@ -1,3 +1,4 @@
+@tool
 extends Resource
 class_name Item
 
@@ -9,3 +10,12 @@ var id := "":
 @export var weight : int = 5
 @export var icon : Texture2D
 @export var amount = 1
+
+@export_tool_button("Set Title From Filename")
+var set_title_button := set_title_from_filename
+
+func set_title_from_filename() -> void:
+	if resource_path.is_empty():
+		push_warning("Item must be saved as a .tres file first.")
+		return
+	title = resource_path.get_file().get_basename().capitalize()
