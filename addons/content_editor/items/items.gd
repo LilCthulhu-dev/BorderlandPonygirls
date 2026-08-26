a98da06f53c879027ab0b18de14ccbe68a0205da
LilCthulhu <316244590+LilCthulhu-dev@users.noreply.github.com>
2026-08-26 19:39:23 +0200

    testing editor

---
+++ addons/content_editor/items/items.gd
@@ -0,0 +1,14 @@
+@tool
+extends TabBar
+
+func _ready() -> void:
+	pass
+
+func _on_save_btn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_save_as_btn_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_new_btn_pressed() -> void:
+	pass # Replace with function body.
--- addons/content_editor/plugin.gd
+++ addons/content_editor/plugin.gd
@@ -1,8 +1,15 @@
 @tool
 extends EditorPlugin
 
+const CONTENT_EDITOR = preload("uid://b1rmg7548o7lk")
+var editor: Control
+
 func _enter_tree() -> void:
+	editor = CONTENT_EDITOR.instantiate() as Control
+	add_control_to_dock(DOCK_SLOT_RIGHT_UL, editor)
 	print("Content Editor enabled")
 
 func _exit_tree() -> void:
+	remove_control_from_docks(editor)
+	editor.queue_free()
 	print("Content Editor disabled")
