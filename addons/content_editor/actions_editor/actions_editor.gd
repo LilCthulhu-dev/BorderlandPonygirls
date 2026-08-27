ac51d73ffdac466f539653fa628a9787b73e7f88
LilCthulhu <316244590+LilCthulhu-dev@users.noreply.github.com>
2026-08-27 19:05:59 +0200

    working on actions

---
+++ addons/content_editor/actions_editor/actions_editor.gd
@@ -0,0 +1,14 @@
+extends PanelContainer
+
+@onready var add_container: VBoxContainer = %AddContainer
+@onready var remove_container: VBoxContainer = %RemoveContainer
+@onready var require_container: VBoxContainer = %RequireContainer
+
+func _on_add_picker_resource_changed(resource: Resource) -> void:
+	pass # Replace with function body.
+
+func _on_remove_picker_resource_changed(resource: Resource) -> void:
+	pass # Replace with function body.
+
+func _on_require_picker_resource_changed(resource: Resource) -> void:
+	pass # Replace with function body.
